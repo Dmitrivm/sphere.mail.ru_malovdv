201ac223aad6990b536e8332efc5771a0b17b24a
Dmitrivm <dmitrivm@mail.ru>
2019-04-25 13:10:37 +0300

    One more correction

--- CPP/hw4/serializer.h
+++ CPP/hw4/serializer.h
@@ -87,9 +87,9 @@ public:
     }
 
     template <class... ArgsT>
-    Error operator()(ArgsT&... args)
+    Error operator()(ArgsT&&... args)
     {
-        return process(std::forward<ArgsT&>(args)...);
+        return process(std::forward<ArgsT>(args)...);
     }
     
 private:
